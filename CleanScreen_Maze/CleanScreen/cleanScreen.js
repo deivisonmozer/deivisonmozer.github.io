c3ea9b046e484cd3c775dbee7d63b1487523d36b
deivison m <deivisonmozer@gmail.com>
2022-09-14 20:31:50 -0500

    ajustes

--- CleanScreen_Maze/CleanScreen/cleanScreen.js
+++ CleanScreen_Maze/CleanScreen/cleanScreen.js
@@ -1,33 +1,5 @@
 window.onload = ()=> {
-    let val = 100;
-    let interval = setInterval(fIncreaseSize, 250, val);
-    document.getElementById("idCircle").onclick = removeCircle;
-    document.getElementById("idStop").onclick = ()=>{fstop(interval)};
     document.getElementById("idStart").onclick = start;
-}
-
-function fIncreaseSize(val){
-    let current = document.getElementById("idCircle").style.width
-
-    if(!current)
-        document.getElementById("idCircle").style.width = val + "px";
-    else {
-        document.getElementById("idCircle").style.width = parseInt(current) + 10 + "px";   
-        document.getElementById("idCircle").style.height = parseInt(current) + 10 + "px";   
-    }
-}
-
-function removeCircle(){
-    console.log("clicked");
-    let c = document.getElementById("idCircle");
-    c.remove();
-
-    console.log("removed");
-}
-
-function fstop(x){
-    clearInterval(x);
-
 }
 
 function start(){
@@ -39,20 +11,27 @@ function start(){
     for(let i=0; i<number; i++){
         generateCircle(width, maxGrowth, interval);
     }
-    
-}
+}   
 
 function generateCircle(width, maxGrowth, interval){
     let obj = document.createElement("div");
     obj.id = "idCircle";
-    obj.style.width = width;
+    obj.style.width = width + "px";
+    obj.style.height = width + "px";
     obj.style.maxWidth = maxGrowth + "px"; 
     obj.style.backgroundColor = "rgb("+generateNumber(0, 255)+","+ generateNumber(0, 255) +","+ generateNumber(0, 255) + ")";
-    obj.onclick = removeCircle;
+    obj.onclick = ()=>{obj.remove()};
     document.getElementById("content").appendChild(obj);
+
     //need to create a interval for each element
-    let timer = setInterval(fIncreaseSize, interval, 50);
-    document.getElementById("idStop").onclick = ()=>{fstop(timer)};
+    let timer = setInterval(fIncreaseSizeObj, interval, obj);
+}
+
+function fIncreaseSizeObj(obj){
+    let current = obj.style.width;
+    
+    obj.style.width = parseInt(current) + 10 + "px";   
+    obj.style.height = parseInt(current) + 10 + "px";   
 }
 
 function generateNumber(min, max){
